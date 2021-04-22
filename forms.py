64eb851a282635f94ed426dc83333b8576d6745c
Vinay Bhat <vinayvamanbhat@gmail.com>
2021-04-22 22:14:06 +0530

    Some changes

--- forms.py
+++ forms.py
@@ -3,5 +3,5 @@ from wtforms import SubmitField, StringField, validators
 from wtforms.validators import DataRequired, Length
 
 class InputForm(FlaskForm):
-    Interface = StringField('Interface Name', validators=[DataRequired(), Length(min=2, max=20)])
+    Interface = StringField('Interface Name', validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Enter Interface"})
     submit = SubmitField('Submit')
